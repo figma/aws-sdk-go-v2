cf6d75dfa30899710846b33b874337eed40d145c
Dave Martin <dmartin@figma.com>
2025-08-13 13:39:23 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/s3shared
    Original tag: service/internal/s3shared/v1.18.17
    New tag: service/internal/s3shared/v1.18.17-newtest.18

--- service/internal/s3shared/metadata_retriever.go
+++ service/internal/s3shared/metadata_retriever.go
@@ -3,7 +3,7 @@ package s3shared
 import (
 	"context"
 
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
 	"github.com/aws/smithy-go/middleware"
 	"github.com/aws/smithy-go/tracing"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
