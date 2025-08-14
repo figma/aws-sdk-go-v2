454476e92200d6687c0f782abaeb567e8852f02a
Matthew Finifter <mfinifter@figma.com>
2025-08-14 15:56:45 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/inspector2
    Original tag: service/inspector2/v1.43.0
    New tag: service/inspector2/v1.43.0-multitest-3

--- service/storagegateway/api_op_CreateSMBFileShare.go
+++ service/storagegateway/api_op_CreateSMBFileShare.go
@@ -5,8 +5,8 @@ package storagegateway
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/storagegateway/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/storagegateway/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
