2b760f51ac9aca59541fc9106a99eaf797ea0023
Matthew Finifter <mfinifter@figma.com>
2025-08-14 15:54:43 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding
    Original tag: service/internal/accept-encoding/v1.13.0
    New tag: service/internal/accept-encoding/v1.13.0-multitest-3

--- service/directoryservice/api_op_ListIpRoutes.go
+++ service/directoryservice/api_op_ListIpRoutes.go
@@ -5,8 +5,8 @@ package directoryservice
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/directoryservice/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/directoryservice/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
