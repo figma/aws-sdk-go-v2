e765cf5c56bd92d84bbd8159a11e1ad9c33a55b8
Matthew Finifter <mfinifter@figma.com>
2025-08-14 16:13:34 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding
    Original tag: service/internal/accept-encoding/v1.13.0
    New tag: service/internal/accept-encoding/v1.13.0-figma.multitest-4

--- aws/retry/retryable_error.go
+++ aws/retry/retryable_error.go
@@ -7,7 +7,7 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/aws"
 )
 
 // IsErrorRetryable provides the interface of an implementation to determine if
