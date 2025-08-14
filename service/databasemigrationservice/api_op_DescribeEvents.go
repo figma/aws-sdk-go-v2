d402daf1c7c2e2764bf7f0cc712a5e53a8887392
Matthew Finifter <mfinifter@figma.com>
2025-08-14 15:53:50 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/feature/ec2/imds
    Original tag: feature/ec2/imds/v1.18.3
    New tag: feature/ec2/imds/v1.18.3-multitest-3

--- service/databasemigrationservice/api_op_DescribeEvents.go
+++ service/databasemigrationservice/api_op_DescribeEvents.go
@@ -5,8 +5,8 @@ package databasemigrationservice
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/databasemigrationservice/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/databasemigrationservice/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 	"time"
