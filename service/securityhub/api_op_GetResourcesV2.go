e2af48fd8f4f62777680659e25182afb8af47a39
Matthew Finifter <mfinifter@figma.com>
2025-08-14 16:16:18 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/checksum
    Original tag: service/internal/checksum/v1.8.3
    New tag: service/internal/checksum/v1.8.3-figma.multitest-4

--- service/securityhub/api_op_GetResourcesV2.go
+++ service/securityhub/api_op_GetResourcesV2.go
@@ -5,8 +5,8 @@ package securityhub
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/securityhub/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/securityhub/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
