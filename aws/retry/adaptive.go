7578fb9299b6aae484fc55822fc1ce9a18dabd18
Matthew Finifter <mfinifter@figma.com>
2025-08-14 16:13:46 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/presigned-url
    Original tag: service/internal/presigned-url/v1.13.3
    New tag: service/internal/presigned-url/v1.13.3-figma.multitest-4

--- aws/retry/adaptive.go
+++ aws/retry/adaptive.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/internal/sdk"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/internal/sdk"
 )
 
 const (
