d699689ea2cd5ce92f72307e7d2e024a1524d580
Matthew Finifter <mfinifter@figma.com>
2025-08-14 15:56:21 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/ecs
    Original tag: service/ecs/v1.63.0
    New tag: service/ecs/v1.63.0-multitest-3

--- service/b2bi/api_op_GetTransformerJob.go
+++ service/b2bi/api_op_GetTransformerJob.go
@@ -5,8 +5,8 @@ package b2bi
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/b2bi/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/b2bi/types"
 	"github.com/aws/smithy-go/middleware"
 	smithytime "github.com/aws/smithy-go/time"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
