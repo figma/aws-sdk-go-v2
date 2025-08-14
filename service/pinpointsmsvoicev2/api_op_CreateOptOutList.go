d37e3bb6b9299cc57be393d77445ce549d50a8b8
Matthew Finifter <mfinifter@figma.com>
2025-08-14 16:12:43 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/feature/ec2/imds
    Original tag: feature/ec2/imds/v1.18.3
    New tag: feature/ec2/imds/v1.18.3-figma.multitest-4

--- service/pinpointsmsvoicev2/api_op_CreateOptOutList.go
+++ service/pinpointsmsvoicev2/api_op_CreateOptOutList.go
@@ -5,8 +5,8 @@ package pinpointsmsvoicev2
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/pinpointsmsvoicev2/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/pinpointsmsvoicev2/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 	"time"
