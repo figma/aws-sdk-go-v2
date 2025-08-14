205c94298458af68aaeeb6a2a10a85305252234b
Matthew Finifter <mfinifter@figma.com>
2025-08-14 15:57:36 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/checksum
    Original tag: service/internal/checksum/v1.8.3
    New tag: service/internal/checksum/v1.8.3-multitest-3

--- service/arcregionswitch/deserializers.go
+++ service/arcregionswitch/deserializers.go
@@ -5,7 +5,7 @@ package arcregionswitch
 import (
 	"context"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/arcregionswitch/types"
+	"github.com/figma/aws-sdk-go-v2/service/arcregionswitch/types"
 	smithy "github.com/aws/smithy-go"
 	smithycbor "github.com/aws/smithy-go/encoding/cbor"
 	"github.com/aws/smithy-go/middleware"
--- service/wisdom/api_op_SearchQuickResponses.go
+++ service/wisdom/api_op_SearchQuickResponses.go
@@ -5,8 +5,8 @@ package wisdom
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/wisdom/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/wisdom/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
