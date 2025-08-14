3a2569565c948a7c9b1e65051782b6715ca8d6b1
Matthew Finifter <mfinifter@figma.com>
2025-08-14 16:12:31 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2
    Original tag: v1.38.0
    New tag: v1.38.0-figma.multitest-4

--- feature/s3/transfermanager/api_op_DownloadObject.go
+++ feature/s3/transfermanager/api_op_DownloadObject.go
@@ -10,11 +10,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/transfermanager/types"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/feature/s3/transfermanager/types"
+	"github.com/figma/aws-sdk-go-v2/service/s3"
+	s3types "github.com/figma/aws-sdk-go-v2/service/s3/types"
 	smithymiddleware "github.com/aws/smithy-go/middleware"
 )
 
--- service/ssmguiconnect/validators.go
+++ service/ssmguiconnect/validators.go
@@ -5,7 +5,7 @@ package ssmguiconnect
 import (
 	"context"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/ssmguiconnect/types"
+	"github.com/figma/aws-sdk-go-v2/service/ssmguiconnect/types"
 	smithy "github.com/aws/smithy-go"
 	"github.com/aws/smithy-go/middleware"
 )
