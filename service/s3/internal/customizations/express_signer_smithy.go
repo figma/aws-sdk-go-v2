bd8095b2c8e64d6b595c8a2d0e6888e2f7308205
Matthew Finifter <mfinifter@figma.com>
2025-08-14 15:57:47 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/internal/s3shared
    Original tag: service/internal/s3shared/v1.19.3
    New tag: service/internal/s3shared/v1.19.3-multitest-3

--- service/s3/internal/customizations/express_signer_smithy.go
+++ service/s3/internal/customizations/express_signer_smithy.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
-	internalauthsmithy "github.com/aws/aws-sdk-go-v2/internal/auth/smithy"
-	"github.com/aws/aws-sdk-go-v2/internal/sdk"
+	v4 "github.com/figma/aws-sdk-go-v2/aws/signer/v4"
+	internalauthsmithy "github.com/figma/aws-sdk-go-v2/internal/auth/smithy"
+	"github.com/figma/aws-sdk-go-v2/internal/sdk"
 	"github.com/aws/smithy-go"
 	"github.com/aws/smithy-go/auth"
 	"github.com/aws/smithy-go/logging"
