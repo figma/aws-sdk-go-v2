a6baad379b6e5d7f046545cdeafe390ac0e91bfe
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:07:08 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/ssooidc
    Original tag: service/ssooidc/v1.33.0
    New tag: service/ssooidc/v1.33.0-figma.multitest-5

--- service/marketplacecatalog/api_op_DescribeEntity.go
+++ service/marketplacecatalog/api_op_DescribeEntity.go
@@ -5,8 +5,8 @@ package marketplacecatalog
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/marketplacecatalog/document"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/marketplacecatalog/document"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
--- service/socialmessaging/api_op_AssociateWhatsAppBusinessAccount.go
+++ service/socialmessaging/api_op_AssociateWhatsAppBusinessAccount.go
@@ -5,8 +5,8 @@ package socialmessaging
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/socialmessaging/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/socialmessaging/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
