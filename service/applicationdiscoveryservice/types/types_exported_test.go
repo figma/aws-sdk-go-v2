087b9e1927bae847c64491c7caa9fa3c6ba762dc
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:32:15 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/ec2
    Original tag: service/ec2/v1.243.0
    New tag: service/ec2/v1.243.0-figma.multitest-6

--- service/applicationdiscoveryservice/types/types_exported_test.go
+++ service/applicationdiscoveryservice/types/types_exported_test.go
@@ -4,7 +4,7 @@ package types_test
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/applicationdiscoveryservice/types"
+	"github.com/figma/aws-sdk-go-v2/service/applicationdiscoveryservice/types"
 )
 
 func ExampleExportPreferences_outputUsage() {
--- service/s3/handwritten_paginators.go
+++ service/s3/handwritten_paginators.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/aws"
 )
 
 // ListObjectVersionsAPIClient is a client that implements the ListObjectVersions
