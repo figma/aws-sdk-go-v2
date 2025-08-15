3e311aafd0fa0e5831566ab71e98bc2cd427476b
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:06:43 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/internal/configsources
    Original tag: internal/configsources/v1.4.3
    New tag: internal/configsources/v1.4.3-figma.multitest-5

--- service/licensemanagerlinuxsubscriptions/api_op_RegisterSubscriptionProvider.go
+++ service/licensemanagerlinuxsubscriptions/api_op_RegisterSubscriptionProvider.go
@@ -5,8 +5,8 @@ package licensemanagerlinuxsubscriptions
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/licensemanagerlinuxsubscriptions/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/licensemanagerlinuxsubscriptions/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
