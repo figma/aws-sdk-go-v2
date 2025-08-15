0ec5936fe18b04f442e01f7b15820dc8a8e25e70
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:30:20 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/internal/configsources
    Original tag: internal/configsources/v1.4.3
    New tag: internal/configsources/v1.4.3-figma.multitest-6

--- feature/s3/manager/upload.go
+++ feature/s3/manager/upload.go
@@ -10,12 +10,12 @@ import (
 	"sort"
 	"sync"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/internal/awsutil"
-	internalcontext "github.com/aws/aws-sdk-go-v2/internal/context"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/internal/awsutil"
+	internalcontext "github.com/figma/aws-sdk-go-v2/internal/context"
+	"github.com/figma/aws-sdk-go-v2/service/s3"
+	"github.com/figma/aws-sdk-go-v2/service/s3/types"
 	smithymiddleware "github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
--- service/ec2/internal/customizations/unit_test.go
+++ service/ec2/internal/customizations/unit_test.go
@@ -8,8 +8,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/service/ec2"
 
 	"github.com/aws/smithy-go"
 )
