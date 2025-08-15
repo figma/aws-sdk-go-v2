b6610f9ba22d021756c03d78169978e57e5748b5
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:06:19 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2
    Original tag: v1.38.0
    New tag: v1.38.0-figma.multitest-5

--- service/keyspacesstreams/api_op_GetRecords.go
+++ service/keyspacesstreams/api_op_GetRecords.go
@@ -5,8 +5,8 @@ package keyspacesstreams
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/keyspacesstreams/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/keyspacesstreams/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
