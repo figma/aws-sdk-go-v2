da46f70234f78a5c12fd0851f1014ecc9665c7d9
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:33:22 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/rds
    Original tag: service/rds/v1.103.0
    New tag: service/rds/v1.103.0-figma.multitest-6

--- credentials/stscreds/assume_role_provider_test.go
+++ credentials/stscreds/assume_role_provider_test.go
@@ -6,10 +6,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/aws/aws-sdk-go-v2/service/sts/types"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/figma/aws-sdk-go-v2/service/sts"
+	"github.com/figma/aws-sdk-go-v2/service/sts/types"
 )
 
 type mockAssumeRole struct {
--- service/workspacesthinclient/api_op_DeregisterDevice.go
+++ service/workspacesthinclient/api_op_DeregisterDevice.go
@@ -5,8 +5,8 @@ package workspacesthinclient
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/workspacesthinclient/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/workspacesthinclient/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
