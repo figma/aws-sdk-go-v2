cded954987161b13f0bd119e6b196d674559f075
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:10:22 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/secretsmanager
    Original tag: service/secretsmanager/v1.38.0
    New tag: service/secretsmanager/v1.38.0-figma.multitest-5

--- service/s3/express_default.go
+++ service/s3/express_default.go
@@ -9,9 +9,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/internal/sdk"
-	"github.com/aws/aws-sdk-go-v2/internal/sync/singleflight"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/internal/sdk"
+	"github.com/figma/aws-sdk-go-v2/internal/sync/singleflight"
 	"github.com/aws/smithy-go/container/private/cache"
 	"github.com/aws/smithy-go/container/private/cache/lru"
 )
