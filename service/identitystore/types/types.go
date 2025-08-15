b459c57fef60e8ed976f2d971d74b4135f36b39e
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:08:31 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/ec2
    Original tag: service/ec2/v1.243.0
    New tag: service/ec2/v1.243.0-figma.multitest-5

--- service/identitystore/types/types.go
+++ service/identitystore/types/types.go
@@ -3,7 +3,7 @@
 package types
 
 import (
-	"github.com/aws/aws-sdk-go-v2/service/identitystore/document"
+	"github.com/figma/aws-sdk-go-v2/service/identitystore/document"
 	smithydocument "github.com/aws/smithy-go/document"
 )
 
--- service/s3vectors/api_op_QueryVectors.go
+++ service/s3vectors/api_op_QueryVectors.go
@@ -5,9 +5,9 @@ package s3vectors
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/s3vectors/document"
-	"github.com/aws/aws-sdk-go-v2/service/s3vectors/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/s3vectors/document"
+	"github.com/figma/aws-sdk-go-v2/service/s3vectors/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
