5c5a40575e913cd02d175940e9b2cd874fd4bd66
Matthew Finifter <mfinifter@figma.com>
2025-08-14 17:06:55 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/internal/endpoints/v2
    Original tag: internal/endpoints/v2.7.3
    New tag: internal/endpoints/v2.7.3-figma.multitest-5

--- service/inspector2/api_op_GetCodeSecurityScan.go
+++ service/inspector2/api_op_GetCodeSecurityScan.go
@@ -5,8 +5,8 @@ package inspector2
 import (
 	"context"
 	"fmt"
-	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
-	"github.com/aws/aws-sdk-go-v2/service/inspector2/types"
+	awsmiddleware "github.com/figma/aws-sdk-go-v2/aws/middleware"
+	"github.com/figma/aws-sdk-go-v2/service/inspector2/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 	"time"
