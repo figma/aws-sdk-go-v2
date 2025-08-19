7c42f8bca93747dac7625cd825cacb84f80ad272
Matthew Finifter <mfinifter@figma.com>
2025-08-19 16:27:52 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2/service/rds
    Original tag: service/rds/v1.103.0
    New tag: service/rds/v1.103.0-newtest.1

--- config/shared_config_test.go
+++ config/shared_config_test.go
@@ -11,9 +11,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
-	"github.com/aws/aws-sdk-go-v2/internal/ini"
+	"github.com/figma/aws-sdk-go-v2/aws"
+	"github.com/figma/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/figma/aws-sdk-go-v2/internal/ini"
 	"github.com/aws/smithy-go/logging"
 	"github.com/aws/smithy-go/ptr"
 )
