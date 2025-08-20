f0c057605dcff020a6b4f82f872a31541fc21e71
Matthew Finifter <mfinifter@figma.com>
2025-08-20 10:46:34 -0700

    Rewrite imports from github.com/aws/aws-sdk-go-v2 to github.com/figma/aws-sdk-go-v2
    Module: github.com/aws/aws-sdk-go-v2
    Original tag: v1.38.0
    New tag: v1.38.0-figma.20250820

--- service/internal/checksum/middleware_compute_input_checksum_test.go
+++ service/internal/checksum/middleware_compute_input_checksum_test.go
@@ -16,8 +16,8 @@ import (
 	"testing"
 	"testing/iotest"
 
-	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
-	internalcontext "github.com/aws/aws-sdk-go-v2/internal/context"
+	v4 "github.com/figma/aws-sdk-go-v2/aws/signer/v4"
+	internalcontext "github.com/figma/aws-sdk-go-v2/internal/context"
 	"github.com/aws/smithy-go/logging"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
